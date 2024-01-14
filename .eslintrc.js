339e079ceee3b2a6aba7fc67f54efe80daf1cc0c
bigacz <sbigacz@gmail.com>
2024-01-14 23:07:44 +0100

    initialize repo

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'no-use-before-define': 'off',
+    'no-unused-vars': 'off',
+    'consistent-return': 'off',
+    'no-console': 'off',
+    'max-classes-per-file': 'off',
+    'import/extensions': 'off',
+    'prefer-const': 'off',
+    'no-empty': 'off',
+  },
+};
