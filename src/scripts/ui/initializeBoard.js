f5b396cfe834da6e9552761ae51073cffd582fb7
bigacz <sbigacz@gmail.com>
2024-01-18 15:47:43 +0100

    add board dom

--- src/scripts/ui/initializeBoard.js
+++ src/scripts/ui/initializeBoard.js
@@ -1,15 +1,34 @@
 const board = document.getElementById('board');
 
 function initializeBoard() {
-  for (let x = 0; x < 8; x += 1) {
-    for (let y = 0; y < 8; y += 1) {
-      const square = document.createElement('div');
-      square.setAttribute('data-x', x);
-      square.setAttribute('data-y', y);
-
-      board.appendChild(square);
+  for (let y = 0; y < 8; y += 1) {
+    for (let x = 7; x >= 0; x -= 1) {
+      createSquare(x, y);
     }
   }
+}
+
+function createSquare(x, y) {
+  const square = document.createElement('div');
+  square.setAttribute('data-x', x);
+  square.setAttribute('data-y', y);
+
+  const isOddX = x % 2;
+  const isOddY = y % 2;
+
+  if (isOddY) {
+    if (isOddX) {
+      square.classList.add('board_square-black');
+    } else {
+      square.classList.add('board_square-white');
+    }
+  } else if (isOddX) {
+    square.classList.add('board_square-white');
+  } else {
+    square.classList.add('board_square-black');
+  }
+
+  board.prepend(square);
 }
 
 initializeBoard();
